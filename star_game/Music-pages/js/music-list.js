91b846ec31bc74c9bc2ab64caf05fc23307a1d1c
dresyah <121681863+andresyaputra@users.noreply.github.com>
2024-04-04 02:09:36 +0700

    Add files via upload

---
+++ star_game/Music-pages/js/music-list.js
@@ -0,0 +1,21 @@
+
+let allMusic = [
+  {
+    name: "we fell in love in october",
+    artist: "girl in red",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "BIBI(비비) _ KAZINO",
+    artist: "R&B",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "I LOVE YOU SO",
+    artist: "The Walters",
+    img: "music-3",
+    src: "music-3"
+  },
+];
